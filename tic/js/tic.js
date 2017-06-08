12c1f816a41a2b05509e279433c4f1ac6ab5672c
ravenusmc <mcuddy77@gmail.com>
2017-06-08 15:08:07 -0400

    game board finished

---
+++ tic/js/tic.js
@@ -0,0 +1,16 @@
+var game_over = false
+
+while (game_over = false){
+
+    document.querySelector("#square-one").addEventListener('click', function(){
+        document.querySelector('.one_square').textContent = 'X';   
+    });
+
+
+
+}
+
+    document.querySelector("#square-two").addEventListener('click', function(){
+        document.querySelector('.two_square').textContent = 'O';   
+    });
+
