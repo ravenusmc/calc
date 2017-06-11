413cad24f64984bef2b64e7b1d3eebb0c74f5f24
ravenusmc <mcuddy77@gmail.com>
2017-06-11 14:41:21 -0400

    tic toe basically done

--- tic/js/tic.js
+++ tic/js/tic.js
@@ -5,8 +5,6 @@ var game_array = [0,1,2,3,4,5,6,7,8]
 //This Function will ensure that player one is highlighted green when the game starts. 
 function start(){
     document.querySelector('.p_one').classList.add('current_player');
-    var test = document.querySelector('.one_square').textContent;
-    console.log(test)
 }
 
 
@@ -202,7 +200,18 @@ function check(){
             (game_array[0] == 'O' && game_array[4] == 'O' && game_array[8] == 'O') ||
             (game_array[2] == 'O' && game_array[4] == 'O' && game_array[6] == 'O')) {
         document.querySelector('.p_two').innerHTML = "Winner!!";
+    }else if (
+        (game_array[0] == 'X' || game_array[0] == 'O') && (game_array[1] == 'X' || game_array[1] == 'O') &&
+        (game_array[2] == 'X' || game_array[2] == 'O') && (game_array[3] == 'X' || game_array[3] == 'O') &&
+        (game_array[4] == 'X' || game_array[4] == 'O') && (game_array[5] == 'X' || game_array[5] == 'O') &&
+        (game_array[6] == 'X' || game_array[6] == 'O') && (game_array[7] == 'X' || game_array[7] == 'O') &&
+        (game_array[8] == 'X' || game_array[8] == 'O')
+    ){
+        debugger;
+        alert('Tie game!!!');
+        reset();
     }
+
 }
 
 //This function moves to the next player. 
@@ -221,33 +230,38 @@ function nextPlayer(){
     }
 }
 
-//Here I allow the players to reset the board. 
-document.querySelector('#reset').addEventListener('click', function(){
+//This function will reset the game board. 
+function reset(){
 
-    //Changing the squares back to their original values. 
-    document.querySelector('.one_square').textContent = '1';
-    document.querySelector('.two_square').textContent = '2';
-    document.querySelector('.three_square').textContent = '3';
-    document.querySelector('.four_square').textContent = '4';
-    document.querySelector('.five_square').textContent = '5';
-    document.querySelector('.six_square').textContent = '6';
-    document.querySelector('.seven_square').textContent = '7';
-    document.querySelector('.eight_square').textContent = '8';
-    document.querySelector('.nine_square').textContent = '9';
+        //Changing the squares back to their original values. 
+        document.querySelector('.one_square').textContent = '1';
+        document.querySelector('.two_square').textContent = '2';
+        document.querySelector('.three_square').textContent = '3';
+        document.querySelector('.four_square').textContent = '4';
+        document.querySelector('.five_square').textContent = '5';
+        document.querySelector('.six_square').textContent = '6';
+        document.querySelector('.seven_square').textContent = '7';
+        document.querySelector('.eight_square').textContent = '8';
+        document.querySelector('.nine_square').textContent = '9';
 
-    //Changing the player logos back to the right color.
-    document.querySelector('.p_two').classList.add('current_player');
-    document.querySelector('.p_one').classList.remove('current_player');
-    document.querySelector('.p_one').classList.add('player_label');
-    document.querySelector('.p_one').classList.add('current_player');
-    document.querySelector('.p_two').classList.remove('current_player');
-    document.querySelector('.p_two').classList.add('player_label');
+        //Changing the player logos back to the right color.
+        document.querySelector('.p_two').classList.add('current_player');
+        document.querySelector('.p_one').classList.remove('current_player');
+        document.querySelector('.p_one').classList.add('player_label');
+        document.querySelector('.p_one').classList.add('current_player');
+        document.querySelector('.p_two').classList.remove('current_player');
+        document.querySelector('.p_two').classList.add('player_label');
 
-    //Changing the player text back to the right text. 
-    document.querySelector('.p_one').innerHTML = "Player One";
-    document.querySelector('.p_two').innerHTML = "Player Two";
+        //Changing the player text back to the right text. 
+        document.querySelector('.p_one').innerHTML = "Player One";
+        document.querySelector('.p_two').innerHTML = "Player Two";
 
-})
+        //Ensuring that the player is set back to one and that the game array is reset as well. 
+        player = 'one';
+        game_array = [0,1,2,3,4,5,6,7,8];
+}
+
+
 
 window.start()
 
