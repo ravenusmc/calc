f3cb9172f3354e984f601a99712e882be8c94320
ravenusmc <mcuddy77@gmail.com>
2017-06-13 09:52:46 -0400

    working on math operand functions

--- calc/js/calc.js
+++ calc/js/calc.js
@@ -1,66 +1,158 @@
-// document.querySelector('.seven').addEventListener('click', function(){
-//    var value_one = document.querySelector('.seven').textContent;
-//    document.querySelector('.input_screen').textContent = value_one;
-// })
-
+//Creating global variables
 var numArray = []
 var nums = []
+var multiply_it, add_it, subtract, divide;
+multiply_it = false;
+add_it = false;
+subtract = false;
+divide = false;
 
 //This array will hold all the numbers each time the user pushes an operand. 
 var numberContainer = [];
 
+//The below functions are for Operands 
 
 document.querySelector('#add_btn').addEventListener('click', function(){
-  test = numArray.join('');
-  test = Number(test);
-  numberContainer.push(test);
-  // for(var i = 0; i < numArray.length; i++){
-  //   num = numArray[i];
-  //   num = Number(num);
-  //   nums.push(num);
-  // }
+  add_it = true
+  displayNum = numArray.join('');
+  displayNum = Number(displayNum);
+  numberContainer.push(displayNum);
+
   //clearing out the numArray so that the program does not crash. 
   numArray = [];
 });
 
+document.querySelector('#mul_btn').addEventListener('click', function(){
+  multiply_it = true;
+  displayNum = numArray.join('');
+  displayNum = Number(displayNum);
+  numberContainer.push(displayNum);
+  //clearing out the numArray so that the program does not crash. 
+  numArray = [];
+})
+
 document.querySelector('#eql_btn').addEventListener('click', function(){
-  total = add(numberContainer);
+  displayNum = numArray.join('');
+  displayNum = Number(displayNum);
+  numberContainer.push(displayNum);
+  if (multiply_it == true){
+    total = multiply(numberContainer);
+  }else if (add_it == true){
+    total = add(numberContainer);
+  }
   document.querySelector('.input_screen').textContent = total;
 })
+
+
+//All the code for the button functions here. I will right comments in the first one but the rest are the same. 
+
+function zero(){
+  var value = document.querySelector('.zero').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
+
+function one(){
+  var value = document.querySelector('.one').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
+
+function two(){
+  var value = document.querySelector('.two').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
+
+function three(){
+  var value = document.querySelector('.three').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
+
+function four(){
+  var value = document.querySelector('.four').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
+
+function five(){
+  var value = document.querySelector('.five').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
+
+function six(){
+  var value = document.querySelector('.six').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
+
 
 function seven(){
   var value = document.querySelector('.seven').textContent;
   value = Number(value)
   numArray.push(value);
   //This line of code will display the values on the screen without a comma. 
-  test = numArray.join('');
-  document.querySelector('.input_screen').textContent = test;
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
 }
-
-function add() {
-  total = 0
-  for (i = 0; i < numberContainer.length; i++){
-    total = numberContainer[i] + total;
-  }
-  return total;
-}
-
-
 
 function eight(){
   var value = document.querySelector('.eight').textContent;
   value = Number(value)
   numArray.push(value);
-  // numArray = numArray.join('');
-  document.querySelector('.input_screen').textContent = numArray;
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
 }
 
-//Place numbers in an array and then display them????
+function nine(){
+  var value = document.querySelector('.nine').textContent;
+  value = Number(value)
+  numArray.push(value);
+  //This line of code will display the values on the screen without a comma. 
+  displayNum = numArray.join('');
+  document.querySelector('.input_screen').textContent = displayNum;
+}
 
-// I have to make the numbers become one number in an array-combine them and then display that. 
+//Problem each time I click the seven function I fire it off which creates an extra 7.
 
-//Join the values in an array after operator button is pushed. 
+function add() {
+  total = 0
+  for (i = 0; i < numberContainer.length; i++){
+    total = numberContainer[i] + total; 
+  }
+  return total;
+}
+
+function multiply() {
+  total = 1
+  for (i = 0; i < numberContainer.length; i++){
+    total = numberContainer[i] * total; 
+  }
+  return total;
+}
 
 
-
-//set up a first number that does everything then switch over to a second number
