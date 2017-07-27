a2678e3d28af5acb9fdbc62c02b90ff60bc48efb
ravenusmc <mcuddy77@gmail.com>
2017-07-27 18:31:34 -0400

    added validation

--- weight/js/weight.js
+++ weight/js/weight.js
@@ -1,8 +1,11 @@
 document.querySelector('#weight_value').addEventListener('click', function(){
   let target = document.querySelector('#weight').value;
 
+  if (target == ''){
+    alert('if');
+    alert("A value must be entered!");
+  }
   let converted_value = target * 0.45359237
-
   document.querySelector('#results_area').textContent = converted_value + ' kilograms';
 
 })
