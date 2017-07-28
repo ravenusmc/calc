70c5f318c9e3797d229374f250dea68be1898e31
ravenusmc <mcuddy77@gmail.com>
2017-07-28 17:36:55 -0400

    added more to the conditional statement

--- weight/js/weight.js
+++ weight/js/weight.js
@@ -1,12 +1,20 @@
 document.querySelector('#weight_value').addEventListener('click', function(){
-  let target = document.querySelector('#weight').value;
+
+  let target = Number(document.querySelector('#weight').value);
 
   if (target == ''){
-    alert('if');
     alert("A value must be entered!");
+  }else {
+    //debugger;
+    let converted_value = target * 0.45359237;
+    if (isNaN(converted_value)){
+      alert('Please enter in a number!');
+    }else {
+      document.querySelector('#results_area').textContent = converted_value + ' kilograms';
+    }
+    
   }
-  let converted_value = target * 0.45359237
-  document.querySelector('#results_area').textContent = converted_value + ' kilograms';
+
 
 })
 
