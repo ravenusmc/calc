6212ce82a890f165dad71cfaaa600100a876cf5f
ravenusmc <mcuddy77@gmail.com>
2017-07-28 22:01:11 -0400

    created option to allow user to select either pounds or kilograms

--- weight/js/weight.js
+++ weight/js/weight.js
@@ -1,21 +1,46 @@
+//This function will return the convert weight to the user. It will also do some input validation 
+//to ensure that only the correct input is entered. 
 document.querySelector('#weight_value').addEventListener('click', function(){
 
   let target = Number(document.querySelector('#weight').value);
+  //debugger;
 
-  if (target == ''){
-    alert("A value must be entered!");
-  }else {
-    //debugger;
-    let converted_value = target * 0.45359237;
-    if (isNaN(converted_value)){
-      alert('Please enter in a number!');
+  if (document.getElementById("lbs").checked == true){
+
+    if (target == ''){
+      alert("A value must be entered!");
+      location.reload();
+    }else if(target < 0){
+      alert('Weight must be a positive value');
+      location.reload();
     }else {
-      document.querySelector('#results_area').textContent = converted_value + ' kilograms';
+      let converted_value = target * 0.45359237;
+      if (isNaN(converted_value)){
+        alert('Please enter in a number!');
+        location.reload();
+      }else {
+        converted_value = (converted_value).toFixed(2);
+        document.querySelector('#results_area').textContent = converted_value + ' kilograms';
+      } 
     }
-    
-  }
-
-
+  }else if (document.getElementById("kgs").checked == true) {
+    if (target == ''){
+      alert("A value must be entered!");
+      location.reload();
+    }else if(target < 0){
+      alert('Weight must be a positive value');
+      location.reload();
+    }else {
+      let converted_value = target * 2.20462;
+      if (isNaN(converted_value)){
+        alert('Please enter in a number!');
+        location.reload();
+      }else {
+        converted_value = (converted_value).toFixed(2);
+        document.querySelector('#results_area').textContent = converted_value + ' kilograms';
+      } 
+    }
+  } 
 })
 
 
