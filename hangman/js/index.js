11ef7bcd67a6b1bbc9a3bbfb4e23bd371a4f52b2
ravenusmc <mcuddy77@gmail.com>
2017-08-25 14:11:41 -0400

    added code for the player to win as well as refractured some code

--- hangman/js/index.js
+++ hangman/js/index.js
@@ -3,7 +3,6 @@
 //Global variables for use in the program. 
 var gameStart = false;
 var word; 
-var wrong_count = 0;
 var right_count = 0; 
 var bodyPartCount = 0;
 var wordCount = 6;
@@ -26,16 +25,18 @@ function chooseWord(){
   word = wordList[random_value];
 
   //I am only showing the word in production mode. 
-  // alert(word)
+  alert(word)
 
   //This variable will ensure that the user starts the game.
   gameStart = true;
 }
 
-//This function will control what the user enters into the program. 
+//This function will control what the user enters into the program. It is really the main function of the 
+//program. Every time the user submits a letter, this function will see if the letter is valid or not, 
+//and also see if the user has won the game. 
 function getValue(word){
 
-  if (gameStart){
+  if (gameStart && right_count <= 5){
 
     //I take the main word and put it into an array using the split method. 
     wordArray = word.split("");
@@ -52,36 +53,41 @@ function getValue(word){
 
     //This is getting the value that the user enters. 
     let target = document.querySelector('#guessValue').value;
-    type = typeof target
+    type = typeof target;
 
+    //Conditional statement to determine when the character made a right or wrong choice. 
     if (word.includes(target)){
 
       //Here I just let the user know that they guessed a right letter. 
       alert('Right Guess!');
 
-      //I look for the index of the letter in the array. 
-      //letterSpot = wordArray.indexOf(target);
-
       //I use those for look to not only place values on the screen if they are in the array but 
       //also to place values if a word has a duplicate. 
-      for (var i = 0; i < wordArray.length; i++){
-        if (wordArray[i] == target){
-          document.getElementById(right[i] + "_correct").innerHTML = target;
-        }
-      }
+      right_count = correctGuess(target, wordArray, right, right_count);
+
+      //This function will check to see if the player won. 
+      win(right_count, word);
 
     }else {
       alert('Wrong Guess!')
-      document.getElementById(wrong[wrong_count]).innerHTML = target;
+
+      //This line is what will place the wrong letter in the used areas location. 
+      document.getElementById(wrong[bodyPartCount]).innerHTML = target;
+
       //This line I make one of the body parts appear on the screen. 
       document.getElementById(bodyParts[bodyPartCount]).setAttribute("style", "display: inline;");
-      wrong_count += 1;
-      if (wrong_count >= 2 && wrong_count < 4){
+
+      if (bodyPartCount >= 2 && bodyPartCount < 4){
         document.getElementById('used_word_area').setAttribute("style", "background-color: yellow;");
-      }else if (wrong_count >= 4){
-        document.getElementById('used_word_area').setAttribute("style", "background-color: pink;");
+      }else if (bodyPartCount >= 4){
+        document.getElementById('used_word_area').setAttribute("style", "background-color: orange;");
+      }else if (bodyPartCount >= 5){
+        document.getElementById('used_word_area').setAttribute("style", "background-color: red;");
       }
+
+      //This variable essentially keeps track of each wrong guess that the player makes. 
       bodyPartCount += 1;
+
       //Calling the function to alert the user that they lost. 
       lose(bodyPartCount);
     }
@@ -89,6 +95,7 @@ function getValue(word){
     alert('You must hit game start button!');
   }
 }
+
 
 /////// FUNCTIONS TO HELP OUT THE MAIN CODE HERE ////////////
 
@@ -102,14 +109,37 @@ function reset(){
 function lose(bodyPartCount){
   if (bodyPartCount == 6){
     alert('You Lose!');
+    alert('The game will now reset');
     location.reload();
+  }
+}
+
+//This function will determine where the correct letters will be placed. 
+function correctGuess(target, wordArray, right, right_count){
+  for (var i = 0; i < wordArray.length; i++){
+    if (wordArray[i] == target){
+      document.getElementById(right[i] + "_correct").innerHTML = target;
+      right_count += 1; 
+    }
+  }
+  return right_count;
+}
+
+
+//This function will tell the player that they won. 
+function win(right_count, word){
+  if (right_count >= 6){
+      alert('You won and saved the hanging man!!!');
+      alert('The word was: ' + word);
+      alert('The game will now reset!');
+      location.reload();
   }
 }
 
 
 
 
-//Scrap Code 
+//Scrap Code-this is code that I was using in my project at one point but decided to take it out. 
 
 //This function will control what the user enters into the program. 
 // document.querySelector('#enteredValue').addEventListener('click', function(){
@@ -146,3 +176,6 @@ function lose(bodyPartCount){
 //I then user the variable letterSpot to place the wrong letter in the correct location. 
 // document.getElementById(right[letterSpot] + "_correct").innerHTML = target;
 
+
+//I look for the index of the letter in the array. 
+//letterSpot = wordArray.indexOf(target);
