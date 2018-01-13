dfdf361facf1e3ff02c8c331cc0424646432cc2e
ravenusmc <mcuddy77@gmail.com>
2018-01-12 19:04:20 -0500

    minor changes

--- mole/assets/js/index.js
+++ mole/assets/js/index.js
@@ -14,3 +14,28 @@ function switchPic(){
 
 //This function will repeat 
 // setInterval(function(){ alert("Hello"); }, 3000);
+
+
+
+function changeTwo() {
+
+  let targetTwo = document.getElementById('two');
+
+  if (targetTwo.style.color == 'black'){
+    document.getElementById('two').src = 'https://source.unsplash.com/AZMmUy2qL6A/255x200';
+    document.getElementById('headingTwo').innerHTML = 'A Group of Friends';
+    targetTwo.style.color = 'blue';
+  }else{
+    document.getElementById('two').src = "https://source.unsplash.com/K2u71wv2eI4/255x200";
+    document.getElementById('headingTwo').innerHTML = 'A Lone individual...';
+    targetTwo.style.color = 'black';
+  }
+}
+
+<div class="card">
+  <img id='two' onclick='changeTwo()' style="color:black;" class="card-img-top" src="https://source.unsplash.com/K2u71wv2eI4/255x200" alt="Card image cap">
+    <div class="card-block">
+      <h4 id='headingTwo' class="card-title">A Lone individual...</h4>
+      <p id="two-paragraph"class="card-text">He who wonders about his journey in life...</p>
+    </div>
+</div>
